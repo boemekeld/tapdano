7fc332e9aa0dba58f4feae8b292a2b8b2b063368
Lucas Boemeke <lucas@blueshift.cc>
2023-09-30 03:35:10 -0300

    up

--- script.js
+++ script.js
@@ -13,14 +13,20 @@ document.addEventListener('DOMContentLoaded', () => {
 
   document.querySelector("#readButton").addEventListener("click", async () => {
     try {
+      alert('aki 1');
       const reader = new NDEFReader();
+      alert('aki 2');
       await reader.scan();
-
+      alert('aki 3');
       reader.onreading = event => {
+        alert('aki 4');
         const textRecord = event.message.records.find(record => record.recordType === "text");
+        alert('aki 5');
         if (textRecord) {
+          alert('aki 6');
           alert("Dados lidos:", textRecord.data);
         }
+        alert('aki 7');
       };
     } catch (error) {
       alert(error);
