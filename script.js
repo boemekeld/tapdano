7ff5815337d8d62e0c5cc67886337c743c4ba6ce
Lucas Boemeke <lucas@blueshift.cc>
2023-09-30 03:20:34 -0300

    up

--- script.js
+++ script.js
@@ -1,29 +1,29 @@
 document.addEventListener('DOMContentLoaded', () => {
   document.querySelector("#writeButton").addEventListener("click", async () => {
     try {
-        const writer = new NDEFWriter();
-        await writer.write({
-            records: [{ recordType: "text", data: "Olá, Mundo NFC!" }]
-        });
-        console.log("Escrito com sucesso!");
+      const writer = new NDEFWriter();
+      await writer.write({
+        records: [{ recordType: "text", data: "Olá, Mundo NFC!" }]
+      });
+      alert("Escrito com sucesso!");
     } catch (error) {
-        console.error("Falha ao escrever na etiqueta", error);
+      console.error("Falha ao escrever na etiqueta", error);
     }
   });
 
   document.querySelector("#readButton").addEventListener("click", async () => {
     try {
-        const reader = new NDEFReader();
-        await reader.scan();
+      const reader = new NDEFReader();
+      await reader.scan();
 
-        reader.onreading = event => {
-            const textRecord = event.message.records.find(record => record.recordType === "text");
-            if (textRecord) {
-                console.log("Dados lidos:", textRecord.data);
-            }
-        };
+      reader.onreading = event => {
+        const textRecord = event.message.records.find(record => record.recordType === "text");
+        if (textRecord) {
+          alert("Dados lidos:", textRecord.data);
+        }
+      };
     } catch (error) {
-        console.error("Falha ao ler a etiqueta", error);
+      console.error("Falha ao ler a etiqueta", error);
     }
   });
 });
